7792a03d8980453757ec6fa44e4e928336f48229
CharlesPark <apdlfdldjqtm@gmail.com>
2023-09-23 16:57:45 +0900

    # 주사위 숫자 표시

--- androidProject_125/app/src/main/java/com/example/androidproject_125/MainActivity.java
+++ androidProject_125/app/src/main/java/com/example/androidproject_125/MainActivity.java
@@ -17,28 +17,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         roll = (ImageView) findViewById(R.id.roll);
+        num = (TextView) findViewById(R.id.textv);
     }
 
     public void onClick(View view){
-        int num = (int)(Math.random()*50);
+        int rollnum = (int)(Math.random()*50);
+        String ran = rollnum+"";
         int Id = view.getId();
-        switch (num){
-            case 0:
+        switch (rollnum){
+            case 1:
+                num.setText(ran);
                 roll.setImageResource(R.drawable.one);
                 break;
-            case 1:
+            case 2:
+                num.setText(ran);
                 roll.setImageResource(R.drawable.two);
                 break;
-            case 2:
+            case 3:
+                num.setText(ran);
                 roll.setImageResource(R.drawable.three);
                 break;
-            case 3:
+            case 4:
+                num.setText(ran);
                 roll.setImageResource(R.drawable.four);
                 break;
-            case 4:
+            case 5:
+                num.setText(ran);
                 roll.setImageResource(R.drawable.five);
                 break;
-            case 5:
+            case 6:
+                num.setText(ran);
                 roll.setImageResource(R.drawable.six);
                 break;
         }
